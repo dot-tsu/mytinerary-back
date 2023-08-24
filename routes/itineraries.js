00b869d6bd259c38fd878fbffa071acc8333fe68
Luca Di Marco <lucanahtsu@gmail.com>
2023-08-24 16:16:23 -0300

    Fix getItinerariesByPlace not working

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getAllItineraries, getItineraryByCity, getItineraryById, createItinerary, updateItinerary, deleteItinerary } from '../controllers/itineraries.js';
+import { getAllItineraries, getItinerariesByPlace, getItineraryById, createItinerary, updateItinerary, deleteItinerary } from '../controllers/itineraries.js';
 
 const router = express.Router();
 
 router.get('/', getAllItineraries);
-router.get('/city/:city', getItineraryByCity); 
+router.get('/place/:placeId', getItinerariesByPlace); 
 router.get('/:id', getItineraryById);
 router.post('/', createItinerary);
 router.put('/:id', updateItinerary);
