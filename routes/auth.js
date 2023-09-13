91ce471c8b36c110f0aca1f42e1c2395d8e759e0
Luca Di Marco <lucanahtsu@gmail.com>
2023-09-13 19:19:55 -0300

    Add google auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createUser, loginUser } from '../controllers/auth.js'; 
+import { createUser, loginUser, googleSignin } from '../controllers/auth.js'; 
 
 const router = express.Router();
 
 router.post('/register', createUser);
 router.post('/login', loginUser); 
+router.post('/google', googleSignin);
 
 export default router;
