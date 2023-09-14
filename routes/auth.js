8447ea393010ff4e51886769dcc9f5f8bf4367ab
Luca Di Marco <lucanahtsu@gmail.com>
2023-09-14 00:16:23 -0300

    Add getUser

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { createUser, loginUser, googleSignin } from '../controllers/auth.js'; 
+import { authenticateJWT } from '../middlewares/authenticateJWT.js';
 
 const router = express.Router();
 
 router.post('/register', createUser);
 router.post('/login', loginUser); 
 router.post('/google', googleSignin);
+router.get('/profile', authenticateJWT, getUserProfile); 
 
 export default router;
